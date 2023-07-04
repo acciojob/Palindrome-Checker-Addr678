a76325ae80cbcac2056267c26af36da02fdb4d7c
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-04 13:01:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+ // Convert the string to lowercase and remove non-alphanumeric characters
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+  // Check if the reversed string is equal to the original string
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
